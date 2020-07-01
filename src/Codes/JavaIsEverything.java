4a9eda24f445ca4a598557dc7fa39fcc83089979
ahmad <ahmad@LAPTOP-O4HD71AE.fios-router.home>
2020-06-30 22:44:13 -0400

    Tesla

---
+++ src/Codes/JavaIsEverything.java
@@ -0,0 +1,20 @@
+package Codes;
+
+public class JavaIsEverything {
+	
+	 String model = "Tesla";
+	 double milage = 5000;
+	static String size = "LE";
+	static int year = 2020;
+	
+	public static void main(String[] args) {
+	
+		JavaIsEverything obj = new JavaIsEverything();
+	
+			System.out.println("Model of Car: "+ obj.model);
+			System.out.println("Milage of Car: "+ obj.milage );
+			System.out.println("Size of Car: "+ size);
+			System.out.println("Year of Car: " + year);
+		}
+	}	
+
